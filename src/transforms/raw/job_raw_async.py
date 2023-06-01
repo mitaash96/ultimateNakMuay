1c587d005f96be828bb83c7ed2748e25f52546a1
mitaash96 <paulmitaash@gmail.com>
2023-06-01 21:25:08 +0530

    async payload analysis

--- src/transforms/raw/job_raw_async.py
+++ src/transforms/raw/job_raw_async.py
@@ -1,12 +1,13 @@
 import os
 import json
+import asyncio
 import time
 from .config_raw import SCRAPED_DATA_DEPENDENCIES, SCRAPED_DATA_TRANSFORMS
 from .utils_raw import saveFile
 init_time = time.time()
 
 
-def generate_json_files(data):
+async def generate_json_files(data):
     input_dict = SCRAPED_DATA_DEPENDENCIES[data]
     filename = SCRAPED_DATA_DEPENDENCIES[data]["output"]
     fileformat = filename.split('.')[-1]
@@ -18,5 +19,13 @@ def generate_json_files(data):
     return saveFile(fileFormat=fileformat, filename=filename, file=payload)
 
 
-TRANSFORMS = [generate_json_files(data) for data in list(SCRAPED_DATA_DEPENDENCIES)]
+async def main():
+    print("running async main")
+    TRANSFORMS = [
+        asyncio.create_task(generate_json_files(data)) for data in list(SCRAPED_DATA_DEPENDENCIES)
+        ]
+    await asyncio.gather(*TRANSFORMS)
+
+
+asyncio.run(main())
 print(f"BUILD FINISHED IN: {time.time() - init_time: .4f} seconds")
